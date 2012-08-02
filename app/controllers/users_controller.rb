4c4808ffd884f2aa105a78675f6be16e3d077e3a
Ryan Stuczynski <ryan.stuczynski@gmail.com>
2012-08-02 00:01:46 -0400

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, 	only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user,		only: [:edit, :update]
+  before_filter :admin_user, 		only: :destroy
+	  #Adding the correct user allowed the deletation of 
+	  #signing in user in update end edit
+
   def new
   	@user = User.new
   end
@@ -7,6 +13,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def index
+  	@users = User.paginate(page: params[:page])
+  end
+ 
   def create
 	@user = User.new(params[:user])
 	if @user.save
@@ -18,4 +28,37 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+    	flash[:success] = "Profile updated"
+		sign_in @user
+		redirect_to @user
+    else
+    	render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+
+  private
+    def signed_in_user
+      store_location
+      redirect_to signin_path, notice: "Please sign in." unless signed_in?
+	end
+
+	def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+	end
+
+	def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,57 @@ describe "User pages" do
 
 	subject { page }
 
+	describe "index" do
+	    let(:user) { FactoryGirl.create(:user) }
+		before(:all) { 30.times { FactoryGirl.create(:user) } }
+    	after(:all)  { User.delete_all }
+    
+    	before(:each) do
+      		valid_signin user
+      		visit users_path
+		end
+
+	    it { should have_selector('title', text: 'All users') }
+	    it { should have_selector('h1',    text: 'All users') }
+
+	    it "should list each user" do
+      		User.paginate(page: 1).each do |user|
+        	page.should have_selector('li', text: user.name)
+      		end
+		end
+
+		describe "delete links" do
+			it { should_not have_link('delete')}
+
+			describe "as an admin user" do
+				let(:admin) { FactoryGirl.create(:admin) }
+  				before do
+    				valid_signin admin
+    				visit users_path
+    			end
+
+    			it { should have_link('delete', href: user_path(User.first)) }
+				it "should be able to delete another user" do
+				    expect { click_link('delete') }.to change(User, :count).by(-1)
+				end
+
+  				it { should_not have_link('delete', href: user_path(admin)) }
+			end
+
+			describe "as non-admin user" do
+		      let(:user) { FactoryGirl.create(:user) }
+		      let(:non_admin) { FactoryGirl.create(:user) }
+
+		      before { valid_signin non_admin }
+		      
+		      describe "submitting a DELETE request to the Users#destroy action" do
+		        before { delete user_path(user) }
+		        specify { response.should redirect_to(root_path) }
+			  end 			
+			end
+		end
+	end
+
 	describe "signup page" do
 	    before { visit signup_path }
 		it { should have_selector('h1',		text: 'Sign up') }
@@ -64,4 +115,43 @@ describe "User pages" do
 			end
 		end
 	end
+
+	describe "edit" do
+	    let(:user) { FactoryGirl.create(:user) }
+	    before do
+			valid_signin user
+		    visit edit_user_path(user)
+		end
+	    
+	    describe "page" do
+			it { should have_selector('h1',		text: "Update your profile") }
+			it { should have_selector('title', 	text: "Edit user") }
+			it { should have_link('change', href: 'http://gravatar.com/emails') }
+		end
+		
+		describe "with invalid information" do
+			before { click_button "Save changes" }
+			
+			it { should have_content('error')}
+		end
+
+		describe "with valid information" do
+      		let(:new_name)  { "New Name" }
+      		let(:new_email) { "new@example.com" }
+
+      		before do
+        		fill_in "Name",				with: new_name
+        		fill_in "Email",			with: new_email
+        		fill_in "Password",			with: user.password
+        		fill_in "Confirm Password", with: user.password
+        		click_button "Save changes"
+			end
+      
+      		it { should have_selector('title', text: new_name) }
+      		it { should have_selector('div.alert.alert-success') }
+      		it { should have_link('Sign out', href: signout_path) }
+      		specify { user.reload.name.should  == new_name }
+      		specify { user.reload.email.should == new_email }
+		end
+	end 
 end
